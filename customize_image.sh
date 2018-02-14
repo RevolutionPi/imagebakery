RevolutionPi/imagebakery

commit 655460ab24cc209ee2d3203b7f6fbdb26b7bb231

Update to stretch
- shrink_image.sh:

  The Raspbian image has grown considerably and just removing
  wolfram-engine is no longer sufficient to fit it on a
  3.9 GByte eMMC, so remove chromium-browser and scratch2
  as well.

  The size of the FAT partition has changed once again and the
  ext4 partition no longer starts immediately after the FAT
  partition (there's a few wasted blocks in-between), so adjust
  the partition size calculation.

- customize_image.sh:

  No longer install a custom /boot/config.txt.  Instead,
  revpi-factory-reset prompts for the machine type on first boot
  and adds the appropriate DT overlay to Raspbian's default
  /boot/config.txt.

  Some packages have been renamed, removed or added and some config
  files have been changed or removed.  Adjust accordingly.

Signed-off-by: Lukas Wunner <lukas@wunner.de>

customize_image.sh
@@ -26,7 +26,6 @@ fi
 
 # copy templates
 cp $BAKERYDIR/templates/cmdline.txt $IMAGEDIR/boot
-cp $BAKERYDIR/templates/config.txt $IMAGEDIR/boot
 cp $BAKERYDIR/templates/revpi-aliases.sh $IMAGEDIR/etc/profile.d
 cp $BAKERYDIR/templates/rsyslog.conf $IMAGEDIR/etc
 
@@ -115,7 +114,7 @@ echo 'APT::Install-Recommends "false";' >> $IMAGEDIR/etc/apt/apt.conf
 # download and install missing packages
 chroot $IMAGEDIR apt-get update
 chroot $IMAGEDIR apt-get -y install `egrep -v '^#' $BAKERYDIR/debs-to-download`
-dpkg --root $IMAGEDIR --force-depends --purge pixel-wallpaper
+dpkg --root $IMAGEDIR --force-depends --purge rpd-wallpaper
 chroot $IMAGEDIR apt-get -y install revpi-wallpaper
 chroot $IMAGEDIR apt-get update
 chroot $IMAGEDIR apt-get -y install teamviewer-revpi
@@ -146,10 +145,6 @@ if [ -e $IMAGEDIR/etc/lightdm/lightdm.conf ] ; then
 	sed -r -i -e "s/^autologin-user=/#autologin-user=/"	\
 		$IMAGEDIR/etc/lightdm/lightdm.conf
 fi
-
-# avoid gratuitous errors on first boot
-rm $IMAGEDIR/etc/init.d/apply_noobs_os_config
-rm $IMAGEDIR/etc/rc2.d/S01apply_noobs_os_config
 
 # remove package lists, they will be outdated within days
 rm $IMAGEDIR/var/lib/apt/lists/*Packages

debs-to-remove
@@ -1,5 +1,4 @@
 wolfram-engine
-wolframscript
 
 # LibreOffice
 libreoffice
@@ -12,17 +11,19 @@ libreoffice-common
 libreoffice-core
 libreoffice-draw
 libreoffice-gtk
+libreoffice-gtk2
 libreoffice-impress
 libreoffice-java-common
 libreoffice-math
 libreoffice-report-builder-bin
 libreoffice-sdbc-hsqldb
 libreoffice-style-galaxy
+libreoffice-systray
 libreoffice-writer
 libmwaw-0.3-3
-libcmis-0.4-4
-libwps-0.3-3
-liborcus-0.8-0
+libcmis-0.5-5v5
+libwps-0.4-4
+liborcus-0.11-0
 libcdr-0.1-1
 librevenge-0.0-0
 libfreehand-0.1-1
@@ -34,6 +35,11 @@ libwpd-0.10-10
 libmspub-0.1-1
 libvisio-0.1-1
 libodfgen-0.1-1
+libpagemaker-0.0-0
+libboost-date-time1.61.0
+libboost-date-time1.62.0
+libboost-filesystem1.61.0
+libboost-system1.61.0
 fonts-opensymbol
 python3-uno
 uno-libs3
@@ -46,18 +52,17 @@ dillo
 libfltk1.3
 netsurf-gtk
 netsurf-common
-libmozjs185-1.0
 
 # Games
 minecraft-pi
 python-minecraftpi
 python3-minecraftpi
-penguinspuzzle
 python3-pgzero
 
 # "Scratch is a visual programming tool which allows the user to create
 # animations and games with a drag-and-drop interface."
 scratch
+scratch2
 nuscratch
 squeak-plugins-scratch
 
@@ -70,6 +75,14 @@ libetpan17
 # "Learn to code creatively by composing or performing music in an incredible
 # range of styles from classical & jazz to EDM."
 sonic-pi
+libqt5scintilla2-12v5
+libqt5scintilla2-l10n
+
+# User interface for controlling the JACK sound server
+qjackctl
+libqwt-qt5-6
+libqt5xml5
+libqt5x11extras5
 
 # "FreePats is a project to create a free and open collection of digital
 # instruments for music production."
@@ -78,27 +91,44 @@ freepats
 # "SuperCollider is a platform for audio synthesis and algorithmic composition,
 # used by musicians, artists, and researchers working with sound."
 supercollider-server
+libscsynth1
+libboost-thread1.62.0
 
 # "TiMidity++ is a software synthesizer. It can play MIDI files by converting
 # them into PCM waveform data."
 timidity
 libesd0
 esound-common
+bluealsa
 
 # Documentation not linked from the desktop menu
 debian-reference-en
 debian-reference-common
 
-# Remove duplicate boost libraries, keep only libboost-iostreams1.55.0
-libboost-iostreams1.54.0
-libboost-iostreams1.53.0
-libboost-iostreams1.50.0
-libboost-iostreams1.49.0
+# Remove duplicate boost libraries, keep only libboost-iostreams1.62.0
+libboost-iostreams1.61.0
+libboost-iostreams1.60.0
+libboost-iostreams1.58.0
+
+# Remove duplicate gcc base files, keep only gcc-6-base
+gcc-4.6-base
+gcc-4.7-base
+gcc-4.8-base
+gcc-4.9-base
+gcc-5-base
+
+# Remove header files not required by any other package
+libfreetype6-dev
+libpng-dev
 
 # Remove all fonts not required by any other package
-fonts-sil-gentium-basic
 fonts-dejavu
 fonts-dejavu-extra
+fonts-droid-fallback
+fonts-lato
+fonts-liberation2
+fonts-noto-mono
+fonts-piboto
 xfonts-100dpi
 
 # "This package provides the CMap tables required to display PDF documents
@@ -119,3 +149,6 @@ gtk2-engines
 
 # Collides with the RevPi's hardware RTC
 fake-hwclock
+
+# Unnecessary after installation
+blends-tasks

shrink_image.sh
@@ -15,9 +15,9 @@ partprobe "$SRC_LOOP_DEV"
 mkdir /tmp/src.$$
 mount "$SRC_LOOP_DEV"p2 /tmp/src.$$
 
-# truncate files in wolfram-engine package to release 660 MBytes
+# truncate files in these three packages to release 982 MBytes
 set +x
-dpkg --root /tmp/src.$$ -L wolfram-engine |
+dpkg --root /tmp/src.$$ -L wolfram-engine chromium-browser scratch2 |
 	while read name ; do
 		if [ -f "/tmp/src.$$/$name" ] ; then
 			> "/tmp/src.$$/$name"
@@ -28,14 +28,17 @@ set -x
 # create destination image, the eMMC on CM1 and CM3 has 7634944 sectors
 dd if=/dev/zero of=$2 conv=sparse count=7634944
 
-# determine end sector of FAT partition (usually 92159)
+# determine end sector of FAT partition (usually 93236)
 FAT_END_SECTOR=$(sfdisk --dump $1 | awk '/start=/ {print $4 + $6 - 1; exit}')
 
 # copy partition table and FAT partition
 dd if=$1 of=$2 conv=notrunc count=$FAT_END_SECTOR
 
-# calculate size of ext4 partition (usually 7634944 - 92160 = 7542784)
-EXT4_SIZE=$((7634944 - $FAT_END_SECTOR - 1))
+# determine start sector of ext4 partition (usually 94208)
+EXT4_START_SECTOR=$(sfdisk --dump $1 | awk -F '[ ,]+' '/start=.*type=83/ {print $4}')
+
+# calculate size of ext4 partition (usually 7634944 - 94208 = 7542784)
+EXT4_SIZE=$((7634944 - $EXT4_START_SECTOR))
 
 # shrink ext4 partition to calculated size
 sfdisk --dump $2 | sed -r "/(type|Id)=83\$/s/size=[^,]+/size=$EXT4_SIZE/" |

templates/revpi.list
@@ -1,1 +1,1 @@
-deb http://packages.revolutionpi.de/ jessie main contrib
+deb http://packages.revolutionpi.de/ stretch main contrib

templates/rsyslog.conf
@@ -8,17 +8,17 @@
 #### MODULES ####
 #################
 
-$ModLoad imuxsock # provides support for local system logging
-$ModLoad imklog   # provides kernel logging support
-#$ModLoad immark  # provides --MARK-- message capability
+module(load="imuxsock") # provides support for local system logging
+module(load="imklog")   # provides kernel logging support
+#module(load="immark")  # provides --MARK-- message capability
 
 # provides UDP syslog reception
-#$ModLoad imudp
-#$UDPServerRun 514
+#module(load="imudp")
+#input(type="imudp" port="514")
 
 # provides TCP syslog reception
-#$ModLoad imtcp
-#$InputTCPServerRun 514
+#module(load="imtcp")
+#input(type="imtcp" port="514")
 
 
 ###########################
@@ -72,15 +72,8 @@ user.*				-/var/log/user.log
 # it is easy to write scripts to parse these files.
 #
 #mail.info			-/var/log/mail.info
-mail.warn			-/var/log/mail.warn
-mail.err			/var/log/mail.err
-
-#
-# Logging for INN news system.
-#
-news.crit			/var/log/news/news.crit
-news.err			/var/log/news/news.err
-news.notice			-/var/log/news/news.notice
+#mail.warn			-/var/log/mail.warn
+#mail.err			/var/log/mail.err
 
 #
 # Some "catch-all" log files.
@@ -91,32 +84,10 @@ news.notice			-/var/log/news/news.notice
 *.*;\
 	auth,authpriv.none;\
 	daemon,kern.none;\
-	lpr,user.none;\
-	mail,news.none		-/var/log/messages
+	lpr,mail.none;\
+	user.none		-/var/log/messages
 
 #
 # Emergencies are sent to everybody logged in.
 #
 *.emerg				:omusrmsg:*
-
-#
-# I like to have messages displayed on the console, but only on a virtual
-# console I usually leave idle.
-#
-#daemon,mail.*;\
-#	news.=crit;news.=err;news.=notice;\
-#	*.=debug;*.=info;\
-#	*.=notice;*.=warn	/dev/tty8
-
-# The named pipe /dev/xconsole is for the `xconsole' utility.  To use it,
-# you must invoke `xconsole' with the `-file' option:
-#
-#    $ xconsole -file /dev/xconsole [...]
-#
-# NOTE: adjust the list below, or you'll go crazy if you have a reasonably
-#      busy site..
-#
-#daemon.*;mail.*;\
-#	news.err;\
-#	*.=debug;*.=info;\
-#	*.=notice;*.=warn	|/dev/xconsole

commit 902fee41c22b135b36713aec56396fc4e6ae7587

customize_image: Force HDMI mode even if no HDMI monitor is detected
Our jessie image shipped with a custom config.txt which loaded the
kunbus, i2c1-bcm2708 and i2c-rtc overlays required by the RevPi core.

With stretch, we no longer ship a custom config.txt and instead have
revpi-factory-reset add a single line to load the product-specific
overlay.

However we still need to amend config.txt to force HDMI mode even if no
HDMI monitor is detected.  Otherwise attaching a monitor after booting
would result in a black screen, same if EDID probing fails.

Fixes: 655460ab24cc ("Update to stretch")
Signed-off-by: Lukas Wunner <lukas@wunner.de>

customize_image.sh
@@ -64,6 +64,11 @@ cp $BAKERYDIR/templates/cmdline.txt $IMAGEDIR/boot
 cp $BAKERYDIR/templates/revpi-aliases.sh $IMAGEDIR/etc/profile.d
 cp $BAKERYDIR/templates/rsyslog.conf $IMAGEDIR/etc
 
+# force HDMI mode even if no HDMI monitor is detected
+sed -r -i -e 's/#hdmi_force_hotplug=1/hdmi_force_hotplug=1/' \
+	  -e 's/#hdmi_drive=2/hdmi_drive=2/' \
+	  $IMAGEDIR/boot/config.txt
+
 # limit disk space occupied by logs
 ln -s ../cron.daily/logrotate $IMAGEDIR/etc/cron.hourly
 sed -r -i -e 's/delaycompress/#delaycompress/' \