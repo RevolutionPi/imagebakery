9ccc781f60a790bb281f435709688a0d91195a2d
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-03-17 12:24:42 +0100

    Prevent globbing and word splitting on IMAGEDIR variable
    It is unlikely that the IMAGEDIR variable will contain white spaces. But
    nevertheless use double quote to prevent globbing and word splitting.
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- customize_image.sh
+++ customize_image.sh
@@ -24,26 +24,26 @@ BAKERYDIR=$(dirname "$0")
 LOOPDEVICE=$(losetup -f)
 
 cleanup_umount() {
-	if [ -e $IMAGEDIR ] ; then
-		lsof -t $IMAGEDIR | xargs --no-run-if-empty kill
+	if [ -e "$IMAGEDIR" ] ; then
+		lsof -t "$IMAGEDIR" | xargs --no-run-if-empty kill
 	fi
-	if [ -e $IMAGEDIR/usr/bin/qemu-arm-static ] ; then
-		rm -f $IMAGEDIR/usr/bin/qemu-arm-static
+	if [ -e "$IMAGEDIR/usr/bin/qemu-arm-static" ] ; then
+		rm -f "$IMAGEDIR/usr/bin/qemu-arm-static"
 	fi
-	if mountpoint -q $IMAGEDIR/tmp/debs-to-install ; then
-		umount $IMAGEDIR/tmp/debs-to-install
+	if mountpoint -q "$IMAGEDIR/tmp/debs-to-install" ; then
+		umount "$IMAGEDIR/tmp/debs-to-install"
 	fi
-	if [ -e $IMAGEDIR/tmp/debs-to-install ] ; then
-		rmdir $IMAGEDIR/tmp/debs-to-install
+	if [ -e "$IMAGEDIR/tmp/debs-to-install" ] ; then
+		rmdir "$IMAGEDIR/tmp/debs-to-install"
 	fi
-	if mountpoint -q $IMAGEDIR/boot ; then
-		umount $IMAGEDIR/boot
+	if mountpoint -q "$IMAGEDIR/boot" ; then
+		umount "$IMAGEDIR/boot"
 	fi
-	if mountpoint -q $IMAGEDIR ; then
-		umount $IMAGEDIR
+	if mountpoint -q "$IMAGEDIR" ; then
+		umount "$IMAGEDIR"
 	fi
-	if [ -d $IMAGEDIR ] ; then
-		rmdir $IMAGEDIR
+	if [ -d "$IMAGEDIR" ] ; then
+		rmdir "$IMAGEDIR"
 	fi
 }
 
@@ -89,29 +89,29 @@ fi
 # mount ext4 + FAT filesystems
 losetup "$LOOPDEVICE" "$1"
 partprobe "$LOOPDEVICE"
-mount "$LOOPDEVICE"p2 $IMAGEDIR
-mount "$LOOPDEVICE"p1 $IMAGEDIR/boot
+mount "$LOOPDEVICE"p2 "$IMAGEDIR"
+mount "$LOOPDEVICE"p1 "$IMAGEDIR/boot"
 
 # see https://wiki.debian.org/QemuUserEmulation
 if [ -e /usr/bin/qemu-arm-static ] ; then
-    cp /usr/bin/qemu-arm-static $IMAGEDIR/usr/bin
+    cp /usr/bin/qemu-arm-static "$IMAGEDIR/usr/bin"
 fi
 
 # copy templates
-cp "$BAKERYDIR/templates/cmdline.txt" $IMAGEDIR/boot
-cp "$BAKERYDIR/templates/revpi-aliases.sh" $IMAGEDIR/etc/profile.d
-cp "$BAKERYDIR/templates/rsyslog.conf" $IMAGEDIR/etc
+cp "$BAKERYDIR/templates/cmdline.txt" "$IMAGEDIR/boot"
+cp "$BAKERYDIR/templates/revpi-aliases.sh" "$IMAGEDIR/etc/profile.d"
+cp "$BAKERYDIR/templates/rsyslog.conf" "$IMAGEDIR/etc"
 
 # force HDMI mode even if no HDMI monitor is detected
 sed -r -i -e 's/#hdmi_force_hotplug=1/hdmi_force_hotplug=1/' \
 	  -e 's/#hdmi_drive=2/hdmi_drive=2/' \
-	  $IMAGEDIR/boot/config.txt
+	  "$IMAGEDIR/boot/config.txt"
 
 # limit disk space occupied by logs
-ln -s ../cron.daily/logrotate $IMAGEDIR/etc/cron.hourly
+ln -s ../cron.daily/logrotate "$IMAGEDIR/etc/cron.hourly"
 sed -r -i -e 's/delaycompress/#delaycompress/' \
 	  -e 's/sharedscripts/#sharedscripts/' \
-	  $IMAGEDIR/etc/logrotate.d/rsyslog
+	  "$IMAGEDIR/etc/logrotate.d/rsyslog"
 sed -r -i -e 's/#compress/compress/' -e '2i \
 \
 # limit size of each log file\
@@ -122,58 +122,58 @@ compresscmd /usr/bin/nice\
 compressoptions /usr/bin/xz\
 compressext .xz\
 uncompresscmd /usr/bin/unxz\
-' $IMAGEDIR/etc/logrotate.conf
+' "$IMAGEDIR"/etc/logrotate.conf
 
 # bootstrap apt source, will be overwritten by revpi-repo package
-cp "$BAKERYDIR/templates/revpi.gpg" $IMAGEDIR/etc/apt/trusted.gpg.d
-cp "$BAKERYDIR/templates/revpi.list" $IMAGEDIR/etc/apt/sources.list.d
+cp "$BAKERYDIR/templates/revpi.gpg" "$IMAGEDIR/etc/apt/trusted.gpg.d"
+cp "$BAKERYDIR/templates/revpi.list" "$IMAGEDIR/etc/apt/sources.list.d"
 
 # Move ld.so.preload until installation is finished. Otherwise we get errors
 # from ld.so:
 #   ERROR: ld.so: object '/usr/lib/arm-linux-gnueabihf/libarmmem-${PLATFORM}.so'
 #   from /etc/ld.so.preload cannot be preloaded (cannot open shared object file): ignored.
-mv $IMAGEDIR/etc/ld.so.preload $IMAGEDIR/etc/ld.so.preload.bak
+mv "$IMAGEDIR/etc/ld.so.preload" "$IMAGEDIR/etc/ld.so.preload.bak"
 
 # copy piTest source code
 PICONTROLDIR=`mktemp -d -p /tmp piControl.XXXXXXXX`
 git clone https://github.com/RevolutionPi/piControl $PICONTROLDIR
-cp -pr $PICONTROLDIR/piTest $IMAGEDIR/home/pi/demo
-cp -p $PICONTROLDIR/piControl.h $IMAGEDIR/home/pi/demo
-sed -i -r -e 's%\.\./%%' $IMAGEDIR/home/pi/demo/Makefile
-chown -R 1000:1000 $IMAGEDIR/home/pi/demo
-chmod -R a+rX $IMAGEDIR/home/pi/demo
+cp -pr $PICONTROLDIR/piTest "$IMAGEDIR/home/pi/demo"
+cp -p $PICONTROLDIR/piControl.h "$IMAGEDIR/home/pi/demo"
+sed -i -r -e 's%\.\./%%' "$IMAGEDIR/home/pi/demo/Makefile"
+chown -R 1000:1000 "$IMAGEDIR/home/pi/demo"
+chmod -R a+rX "$IMAGEDIR/home/pi/demo"
 rm -r $PICONTROLDIR
 
 # customize settings
-echo Europe/Berlin > $IMAGEDIR/etc/timezone
-rm $IMAGEDIR/etc/localtime
-echo RevPi > $IMAGEDIR/etc/hostname
-sed -i -e 's/raspberrypi/RevPi/g' $IMAGEDIR/etc/hosts
-if ! grep -qE '^i2c-dev$' $IMAGEDIR/etc/modules ; then
-	echo i2c-dev >> $IMAGEDIR/etc/modules
+echo Europe/Berlin > "$IMAGEDIR/etc/timezone"
+rm "$IMAGEDIR/etc/localtime"
+echo RevPi > "$IMAGEDIR/etc/hostname"
+sed -i -e 's/raspberrypi/RevPi/g' "$IMAGEDIR/etc/hosts"
+if ! grep -qE '^i2c-dev$' "$IMAGEDIR/etc/modules" ; then
+	echo i2c-dev >> "$IMAGEDIR/etc/modules"
 fi
-echo piControl >> $IMAGEDIR/etc/modules
+echo piControl >> "$IMAGEDIR/etc/modules"
 sed -i -r -e 's/^(XKBMODEL).*/\1="pc104"/' \
 	-e 's/^(XKBLAYOUT).*/\1="us"/' \
 	-e 's/^(XKBVARIANT).*/\1=""/' \
-	  $IMAGEDIR/etc/default/keyboard
-sed -i -r -e 's/^(LANG).*/\1="en_US.UTF-8"/' $IMAGEDIR/etc/default/locale
-sed -i -r -e 's/^(# en_US.UTF-8 UTF-8)/en_US.UTF-8 UTF-8/' $IMAGEDIR/etc/locale.gen
-install -d -m 755 -o root -g root $IMAGEDIR/etc/revpi
-echo `basename "$1"` > $IMAGEDIR/etc/revpi/image-release
-install -d -m 700 -o 1000 -g 1000 $IMAGEDIR/home/pi/.ssh
+	  "$IMAGEDIR/etc/default/keyboard"
+sed -i -r -e 's/^(LANG).*/\1="en_US.UTF-8"/' "$IMAGEDIR/etc/default/locale"
+sed -i -r -e 's/^(# en_US.UTF-8 UTF-8)/en_US.UTF-8 UTF-8/' "$IMAGEDIR/etc/locale.gen"
+install -d -m 755 -o root -g root "$IMAGEDIR/etc/revpi"
+echo `basename "$1"` > "$IMAGEDIR/etc/revpi/image-release"
+install -d -m 700 -o 1000 -g 1000 "$IMAGEDIR/home/pi/.ssh"
 
 # activate settings
-chroot $IMAGEDIR dpkg-reconfigure -fnoninteractive keyboard-configuration
-chroot $IMAGEDIR dpkg-reconfigure -fnoninteractive tzdata
-chroot $IMAGEDIR dpkg-reconfigure -fnoninteractive locales
+chroot "$IMAGEDIR" dpkg-reconfigure -fnoninteractive keyboard-configuration
+chroot "$IMAGEDIR" dpkg-reconfigure -fnoninteractive tzdata
+chroot "$IMAGEDIR" dpkg-reconfigure -fnoninteractive locales
 
 # automatically bring up eth0 and eth1 again after a USB bus reset
-sed -i -e '6i# allow-hotplug eth0\n# allow-hotplug eth1\n' $IMAGEDIR/etc/network/interfaces
+sed -i -e '6i# allow-hotplug eth0\n# allow-hotplug eth1\n' "$IMAGEDIR/etc/network/interfaces"
 
 # provide WPA template and prioritize wlan0 routes by default
-sed -i -e '/country=GB/d' $IMAGEDIR/etc/wpa_supplicant/wpa_supplicant.conf
-cat >> $IMAGEDIR/etc/wpa_supplicant/wpa_supplicant.conf <<-EOF
+sed -i -e '/country=GB/d' "$IMAGEDIR/etc/wpa_supplicant/wpa_supplicant.conf"
+cat >> "$IMAGEDIR/etc/wpa_supplicant/wpa_supplicant.conf" <<-EOF
 	
 	# WiFi of Revolutionary Pastries, Inc.
 	network={
@@ -182,7 +182,7 @@ cat >> $IMAGEDIR/etc/wpa_supplicant/wpa_supplicant.conf <<-EOF
 	        key_mgmt=WPA-PSK
 	}
 	EOF
-cat >> $IMAGEDIR/etc/dhcpcd.conf <<-EOF
+cat >> "$IMAGEDIR/etc/dhcpcd.conf" <<-EOF
 	
 	# Prioritize wlan0 routes over eth0 routes.
 	interface wlan0
@@ -190,104 +190,104 @@ cat >> $IMAGEDIR/etc/dhcpcd.conf <<-EOF
 	EOF
 
 # harden network configuration
-chroot $IMAGEDIR /usr/bin/patch /etc/sysctl.conf	\
+chroot "$IMAGEDIR" /usr/bin/patch /etc/sysctl.conf	\
 	< "$BAKERYDIR/templates/sysctl.conf.patch"
 
 # display IP address at login prompt
-sed -i -e '1s/$/ \\4 \\6/' $IMAGEDIR/etc/issue
+sed -i -e '1s/$/ \\4 \\6/' "$IMAGEDIR/etc/issue"
 
 # free up disk space
-dpkg --root $IMAGEDIR --purge `egrep -v '^#' "$BAKERYDIR/debs-to-remove"`
-chroot $IMAGEDIR apt-get -y autoremove --purge
-rm -rf $IMAGEDIR/home/pi/MagPi
+dpkg --root "$IMAGEDIR" --purge `egrep -v '^#' "$BAKERYDIR/debs-to-remove"`
+chroot "$IMAGEDIR" apt-get -y autoremove --purge
+rm -rf "$IMAGEDIR/home/pi/MagPi"
 
 # avoid installing unnecessary packages on this space-constrained machine
-echo 'APT::Install-Recommends "false";' >> $IMAGEDIR/etc/apt/apt.conf
+echo 'APT::Install-Recommends "false";' >> "$IMAGEDIR/etc/apt/apt.conf"
 
 # download and install missing packages
-sed -r -i -e '1ideb http://mirrordirector.raspbian.org/raspbian buster main' $IMAGEDIR/etc/apt/sources.list
-chroot $IMAGEDIR apt-get update
-chroot $IMAGEDIR apt-get -y install apt apt-transport-https libapt-inst2.0 libapt-pkg5.0
-sed -r -i -e '1d' $IMAGEDIR/etc/apt/apt.conf $IMAGEDIR/etc/apt/sources.list
+sed -r -i -e '1ideb http://mirrordirector.raspbian.org/raspbian buster main' "$IMAGEDIR/etc/apt/sources.list"
+chroot "$IMAGEDIR" apt-get update
+chroot "$IMAGEDIR" apt-get -y install apt apt-transport-https libapt-inst2.0 libapt-pkg5.0
+sed -r -i -e '1d' "$IMAGEDIR/etc/apt/apt.conf" "$IMAGEDIR/etc/apt/sources.list"
 
-chroot $IMAGEDIR apt-get -y install `egrep -v '^#' "$BAKERYDIR/debs-to-download"`
-dpkg --root $IMAGEDIR --force-depends --purge rpd-wallpaper
-chroot $IMAGEDIR apt-get -y install revpi-wallpaper
-chroot $IMAGEDIR apt-get update
-chroot $IMAGEDIR apt-get -y install teamviewer-revpi
-chroot $IMAGEDIR apt-mark hold raspi-copies-and-fills
-chroot $IMAGEDIR apt-get -y upgrade
-chroot $IMAGEDIR apt-mark unhold raspi-copies-and-fills
-chroot $IMAGEDIR apt-get clean
+chroot "$IMAGEDIR" apt-get -y install `egrep -v '^#' "$BAKERYDIR/debs-to-download"`
+dpkg --root "$IMAGEDIR" --force-depends --purge rpd-wallpaper
+chroot "$IMAGEDIR" apt-get -y install revpi-wallpaper
+chroot "$IMAGEDIR" apt-get update
+chroot "$IMAGEDIR" apt-get -y install teamviewer-revpi
+chroot "$IMAGEDIR" apt-mark hold raspi-copies-and-fills
+chroot "$IMAGEDIR" apt-get -y upgrade
+chroot "$IMAGEDIR" apt-mark unhold raspi-copies-and-fills
+chroot "$IMAGEDIR" apt-get clean
 
 if [ -e "$IMAGEDIR/etc/init.d/apache2" ] ; then
 	# annoyingly, the postinstall script starts apache2 on fresh installs
-	mount -t proc procfs $IMAGEDIR/proc
-	sed -r -i -e 's/pidof /pidof -x /' $IMAGEDIR/etc/init.d/apache2
-	chroot $IMAGEDIR /etc/init.d/apache2 stop
-	umount $IMAGEDIR/proc
+	mount -t proc procfs "$IMAGEDIR/proc"
+	sed -r -i -e 's/pidof /pidof -x /' "$IMAGEDIR/etc/init.d/apache2"
+	chroot "$IMAGEDIR" /etc/init.d/apache2 stop
+	umount "$IMAGEDIR/proc"
 
 	# configure apache2
-	chroot $IMAGEDIR a2enmod ssl
+	chroot "$IMAGEDIR" a2enmod ssl
 	sed -r -i -e 's/^(\tOptions .*Indexes.*)/#\1/'		\
-		$IMAGEDIR/etc/apache2/apache2.conf
+		"$IMAGEDIR/etc/apache2/apache2.conf"
 fi
 
 # install nodejs and nodered with an install script and revpi-nodes from npm repository
 NODEREDSCRIPT="/tmp/update-nodejs-and-nodered.sh"
 /usr/bin/curl -sL \
 	https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered\
-	--output $IMAGEDIR/$NODEREDSCRIPT
-/usr/bin/chmod 755 $IMAGEDIR/$NODEREDSCRIPT
-chroot $IMAGEDIR /usr/bin/sudo -u pi $NODEREDSCRIPT --confirm-install --confirm-pi
-/usr/bin/rm $IMAGEDIR/$NODEREDSCRIPT
-chroot $IMAGEDIR /usr/bin/npm install --prefix /home/pi/.node-red node-red-contrib-revpi-nodes
+	--output "$IMAGEDIR/$NODEREDSCRIPT"
+/usr/bin/chmod 755 "$IMAGEDIR/$NODEREDSCRIPT"
+chroot "$IMAGEDIR" /usr/bin/sudo -u pi $NODEREDSCRIPT --confirm-install --confirm-pi
+/usr/bin/rm "$IMAGEDIR/$NODEREDSCRIPT"
+chroot "$IMAGEDIR" /usr/bin/npm install --prefix /home/pi/.node-red node-red-contrib-revpi-nodes
 
 # enable ssh daemon by default, disable swap, disable bluetooth on mini-uart
-chroot $IMAGEDIR systemctl enable ssh
-chroot $IMAGEDIR systemctl disable dphys-swapfile
-chroot $IMAGEDIR systemctl disable hciuart
+chroot "$IMAGEDIR" systemctl enable ssh
+chroot "$IMAGEDIR" systemctl disable dphys-swapfile
+chroot "$IMAGEDIR" systemctl disable hciuart
 
 # disable 3rd party software
-chroot $IMAGEDIR systemctl disable logiclab
-chroot $IMAGEDIR systemctl disable nodered
-chroot $IMAGEDIR systemctl disable noderedrevpinodes-server
-chroot $IMAGEDIR systemctl disable revpipyload
+chroot "$IMAGEDIR" systemctl disable logiclab
+chroot "$IMAGEDIR" systemctl disable nodered
+chroot "$IMAGEDIR" systemctl disable noderedrevpinodes-server
+chroot "$IMAGEDIR" systemctl disable revpipyload
 
 # boot to console by default, disable autologin
-chroot $IMAGEDIR systemctl set-default multi-user.target
+chroot "$IMAGEDIR" systemctl set-default multi-user.target
 ln -fs /lib/systemd/system/getty@.service		\
-	$IMAGEDIR/etc/systemd/system/getty.target.wants/getty@tty1.service
-if [ -e $IMAGEDIR/etc/lightdm/lightdm.conf ] ; then
+	"$IMAGEDIR/etc/systemd/system/getty.target.wants/getty@tty1.service"
+if [ -e "$IMAGEDIR/etc/lightdm/lightdm.conf" ] ; then
 	sed -r -i -e "s/^autologin-user=/#autologin-user=/"	\
-		$IMAGEDIR/etc/lightdm/lightdm.conf
+		"$IMAGEDIR/etc/lightdm/lightdm.conf"
 fi
 
 # autologin.conf enables autologin in raspios and raspios-full 
 # but not in raspios-lite
-if [ -e $IMAGEDIR/etc/systemd/system/getty@tty1.service.d/autologin.conf ] ; then
-	rm -f $IMAGEDIR/etc/systemd/system/getty@tty1.service.d/autologin.conf
+if [ -e "$IMAGEDIR/etc/systemd/system/getty@tty1.service.d/autologin.conf" ] ; then
+	rm -f "$IMAGEDIR/etc/systemd/system/getty@tty1.service.d/autologin.conf"
 fi
 
 # peg cpu at 1200 MHz to maximize spi0 throughput and avoid jitter
-chroot $IMAGEDIR /usr/bin/revpi-config enable perf-governor
+chroot "$IMAGEDIR" /usr/bin/revpi-config enable perf-governor
 
 # remove package lists, they will be outdated within days
-rm $IMAGEDIR/var/lib/apt/lists/*Packages
+rm "$IMAGEDIR/var/lib/apt/lists/"*Packages
 
 # install local packages
 if [ "$(/bin/ls "$BAKERYDIR/debs-to-install/"*.deb 2>/dev/null)" ] ; then
-	mkdir $IMAGEDIR/tmp/debs-to-install
-	mount --bind "$BAKERYDIR/debs-to-install" $IMAGEDIR/tmp/debs-to-install
-	chroot $IMAGEDIR sh -c "dpkg -i /tmp/debs-to-install/*.deb"
+	mkdir "$IMAGEDIR/tmp/debs-to-install"
+	mount --bind "$BAKERYDIR/debs-to-install" "$IMAGEDIR/tmp/debs-to-install"
+	chroot "$IMAGEDIR" sh -c "dpkg -i /tmp/debs-to-install/*.deb"
 fi
 
 # remove logs and ssh host keys
-find $IMAGEDIR/var/log -type f -delete
-find $IMAGEDIR/etc/ssh -name "ssh_host_*_key*" -delete
+find "$IMAGEDIR/var/log" -type f -delete
+find "$IMAGEDIR/etc/ssh" -name "ssh_host_*_key*" -delete
 
 # restore ld.so.preload
-mv $IMAGEDIR/etc/ld.so.preload.bak $IMAGEDIR/etc/ld.so.preload
+mv "$IMAGEDIR/etc/ld.so.preload.bak" "$IMAGEDIR/etc/ld.so.preload"
 
 cleanup_umount
 
