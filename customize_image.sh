3b2da05e8c2e405f291cc8aed4f6dabca6b1e1d4
Zhi Han <z.han@kunbus.com>
2022-05-18 16:58:47 +0200

    customize_image.sh: List installed packages at end
    Signed-off-by: Zhi Han <z.han@kunbus.com>

--- customize_image.sh
+++ customize_image.sh
@@ -304,6 +304,9 @@ if [ "$(/bin/ls "$BAKERYDIR/debs-to-install/"*.deb 2>/dev/null)" ] ; then
 	chroot "$IMAGEDIR" sh -c "dpkg -i /tmp/debs-to-install/*.deb"
 fi
 
+# list installed packages
+chroot "$IMAGEDIR" sh -c "dpkg -l"
+
 # remove logs and ssh host keys
 find "$IMAGEDIR/var/log" -type f -delete
 find "$IMAGEDIR/etc/ssh" -name "ssh_host_*_key*" -delete
