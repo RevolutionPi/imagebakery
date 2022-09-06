f4b98e4d2b9a0b95e61a46b39b278367281850b1
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2022-09-06 20:07:36 +0200

    Use systemctl enable for getty@tty1
    Instead of manually creating the link for the getty@tty1.service one
    should use the command `systemctl enable getty@tty1.service` for this.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- customize_image.sh
+++ customize_image.sh
@@ -312,8 +312,7 @@ chroot "$IMAGEDIR" systemctl disable revpipyload
 
 # boot to console by default, disable autologin
 chroot "$IMAGEDIR" systemctl set-default multi-user.target
-ln -fs /lib/systemd/system/getty@.service		\
-	"$IMAGEDIR/etc/systemd/system/getty.target.wants/getty@tty1.service"
+chroot "$IMAGEDIR" systemctl enable getty@tty1.service
 if [ -e "$IMAGEDIR/etc/lightdm/lightdm.conf" ] ; then
 	sed -r -i -e "s/^autologin-user=/#autologin-user=/"	\
 		"$IMAGEDIR/etc/lightdm/lightdm.conf"
