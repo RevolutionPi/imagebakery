RevolutionPi/imagebakery

commit 074b35d7fc804f1bff86ec3c1d332421cc8f1a4c

fix: Remove reference to userconfig.service
The userconfig.service is absent, since the package `userconf-pi` was
removed earlier. Therefore remove any reference to this service.

Fixes: 3f1103da042a ("fix: Removes Raspberry Pi wizards to change `pi` username")
Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

customize_image.sh
@@ -345,10 +345,9 @@ chroot "$IMAGEDIR" systemctl disable raspi-config.service
 
 # Since Raspberry Pi OS Bullseye the default user pi will only be used for the first
 # boot and then replaced by a username which has to be defined in the first boot wizard.
-# Therefore we need to disable the userconfig and set the password to the previous default `raspberry`
-if [[ ! $(grep -q -E '^pi:\*' "$IMAGEDIR/etc/shadow") ]]; then
+# Therefore we set the password to the previous default `raspberry`
+if ! grep -q -E '^pi:\*' "$IMAGEDIR/etc/shadow"; then
 	echo 'pi:raspberry' | chroot "$IMAGEDIR" /usr/sbin/chpasswd
-	chroot "$IMAGEDIR" systemctl disable userconfig
 fi
 
 # Remove banner warning which is shows on every ssh login (present since Bullseye)

commit 6bf6cfaaad30e509cf3fcfd0130e53d18dcc5eed

fix: Set password for user pi unconditionally
Fixes: 074b35d ("fix: Remove reference to userconfig.service")
Reported-by: Ramiro Gsponer <r.gsponer@kunbus.com>
Reported-by: Thomas Böhler <t.boehler@kunbus.com>
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

customize_image.sh
@@ -346,9 +346,7 @@ chroot "$IMAGEDIR" systemctl disable raspi-config.service
 # Since Raspberry Pi OS Bullseye the default user pi will only be used for the first
 # boot and then replaced by a username which has to be defined in the first boot wizard.
 # Therefore we set the password to the previous default `raspberry`
-if ! grep -q -E '^pi:\*' "$IMAGEDIR/etc/shadow"; then
-	echo 'pi:raspberry' | chroot "$IMAGEDIR" /usr/sbin/chpasswd
-fi
+echo 'pi:raspberry' | chroot "$IMAGEDIR" /usr/sbin/chpasswd
 
 # Remove banner warning which is shows on every ssh login (present since Bullseye)
 if [[ -f "$IMAGEDIR/etc/ssh/sshd_config.d/rename_user.conf" ]]; then